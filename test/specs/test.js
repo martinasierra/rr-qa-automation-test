b8793700abc0016482b8506a42e43ee743df0d15
Martina Sierra <martinasierra94@gmail.com>
2021-05-03 17:40:50 -0300

    Add automation test

--- test/specs/test.js
+++ test/specs/test.js
@@ -4,9 +4,18 @@ describe('Google Page', () => {
         return browser.url('https://google.com');
     });
 
-    it('title', () => {
-        browser.pause(2000);
-        expect(browser.getTitle()).toEqual('Google');
-    })
+    it('should introduce Wikipedia in the search bar and press enter', () => {
+        let input = $('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input');
+        input.addValue('Wikipedia');
+        browser.pause(1000);
+        browser.keys("\uE007"); 
+    });
 
+    it('should go to Wikipedia webpage and validate the URL', () => {
+        let = wikiLink =$('#rso > div:nth-child(1) > div:nth-child(1) > div > div > div.yuRUbf > a > h3');
+        wikiLink.click();
+        expect(browser.getUrl()).toEqual('https://es.wikipedia.org/wiki/Wikipedia:Portada');
+        browser.pause(1000);
+    })
 });
+
