54654fd8a3f9b8ffb5e3e71ff093e7a42e72e817
Martina Sierra <martinasierra94@gmail.com>
2021-05-05 15:17:47 -0300

    Change wikipedia link selector

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,21 +1,20 @@
 describe('Google Page', () => {
    
-    beforeAll('open browser', () => {
+    beforeAll('open Google in the browser', () => {
         return browser.url('https://google.com');
     });
 
-    it('should introduce Wikipedia in the search bar and press enter', () => {
+    it('should introduce the text Wikipedia in the search bar and press enter', () => {
         let input = $('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input');
         input.addValue('Wikipedia');
-        browser.pause(1000);
-        browser.keys("\uE007"); 
+        browser.keys("\uE007");
     });
 
     it('should go to Wikipedia webpage and validate the URL', () => {
-        let = wikiLink =$('#rso > div:nth-child(1) > div:nth-child(1) > div > div > div.yuRUbf > a > h3');
+        let = wikiLink =$('.LC20lb.DKV0Md');
         wikiLink.click();
-        expect(browser.getUrl()).toEqual('https://es.wikipedia.org/wiki/Wikipedia:Portada');
         browser.pause(1000);
+        expect(browser.getUrl()).toEqual('https://es.wikipedia.org/wiki/Wikipedia:Portada');
     })
 });
 
